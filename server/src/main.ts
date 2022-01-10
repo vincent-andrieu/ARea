b7e2da835d1199b3202b335d24f515df4c39ff8a
Aurélien Joncour <aurelien.joncour@epitech.eu>
2022-01-10 19:12:57 +0100

    feat(src/main.ts) server express listening

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,1 +1,10 @@
-console.log("OK");
+import * as express from "express";
+
+const app = express();
+const port = process.env.SERVER_PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`Area server is listening on ${port}`);
+}).on("error", (error) => {
+    console.log(error.toString());
+});
