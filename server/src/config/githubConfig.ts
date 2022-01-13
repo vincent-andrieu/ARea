835e1f3d1dd9d12a162df0ba2dec78654235893f
Aurélien Joncour <aurelien.joncour@epitech.eu>
2022-01-13 22:02:03 +0100

    refactor(server/src/config/githubConfig.ts) rename callbackUrl env var

--- server/src/config/githubConfig.ts
+++ server/src/config/githubConfig.ts
@@ -2,10 +2,10 @@ import { env } from "process";
 
 const GITHUB_CLIENT_ID = env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = env.GITHUB_CLIENT_SECRET;
-const CALLBACK_URL = env.CALLBACK_URL;
+const GITHUB_CALLBACK_URL = env.GITHUB_CALLBACK_URL;
 
 export const githubConfig = {
     clientID: GITHUB_CLIENT_ID,
     clientSecret: GITHUB_CLIENT_SECRET,
-    callbackURL: CALLBACK_URL
+    callbackURL: GITHUB_CALLBACK_URL
 };
