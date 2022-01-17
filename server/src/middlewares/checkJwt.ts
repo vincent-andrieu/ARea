b0c6dd69bf22709b28916b361f6fcec538224873
simon.racaud <simon.racaud@epitech.eu>
2022-01-17 14:46:57 +0100

    fix(auth): middleware, extract token from request

--- server/src/middlewares/checkJwt.ts
+++ server/src/middlewares/checkJwt.ts
@@ -2,21 +2,20 @@ import jwt from "jsonwebtoken";
 import { serverConfig } from "../config/serverConfig";
 
 const verifyToken = (req, res, next) => {
-  const token =
-    req.body.token || req.query.token || req.headers["x-access-token"];
+    const token =
+        req.body.token || req.query.token || req.headers["x-access-token"] || req.headers["authorization"].substr(7);
 
-  if (!token) {
-    return res.status(403).send("A token is required for authentication");
-  }
-  try {
-    const decoded = jwt.verify(token, serverConfig.jwtSecret);
-    req.user = decoded;
-  } catch (err) {
-    return res.status(401).send("Invalid Token");
-  }
-  return next();
+    if (!token)
+        return res.status(403).send("A token is required for authentication");
+    try {
+        const decoded = jwt.verify(token, serverConfig.jwtSecret);
+        req.user = decoded;
+    } catch (err) {
+        return res.status(401).send("Invalid Token");
+    }
+    return next();
 };
 
 module.exports = verifyToken;
 
-export default verifyToken
+export default verifyToken;
