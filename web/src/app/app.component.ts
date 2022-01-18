1fd00a5a210e49f649b3c7277030860b1a761e1e
Vincent Andrieu <vincent.andrieu@epitech.eu>
2022-01-18 10:41:26 +0100

    Add angular material & flex-layout

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'web';
-}
+export class AppComponent {}
