223a23d742ebf98c02d4f0e974a094419176d674
Vincent Andrieu <vincent.andrieu@epitech.eu>
2022-01-19 11:09:54 +0100

    Fix server guard

--- web/src/app/guards/server.guard.ts
+++ web/src/app/guards/server.guard.ts
@@ -12,7 +12,7 @@ export class ServerGuard implements CanActivate {
     constructor(private _router: Router, private _cookieService: CookieService) {}
 
     canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-        if (this._cookieService.hasKey(environment.cookiesKey.serverIp))
+        if (!this._cookieService.hasKey(environment.cookiesKey.serverIp))
             return true;
         return this._router.parseUrl('/login');
     }
