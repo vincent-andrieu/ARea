7092a7e283d095c13ca836e8b3f874c48a06ae1e
Arthur <arthur.jourdan@epitech.eu>
2022-02-01 10:21:21 +0100

    fix(setup_bdd.sh): change mongo version from 5 to 4.4

--- server/setup_bdd.sh
+++ server/setup_bdd.sh
@@ -1,1 +1,1 @@
-docker run -d -p 27017:27017 -v area-db-data:/data/db -v area-db-config:/data/configdb --name area-db mongo:5
+docker run -d -p 27017:27017 -v area-db-data:/data/db -v area-db-config:/data/configdb --name area-db mongo:4.4
