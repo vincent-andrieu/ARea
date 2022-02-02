b58e47bb7bcdf02b70c4b0607d88cc42655c6a33
Arthur <arthur.jourdan@epitech.eu>
2022-02-02 16:48:25 +0100

    feat(src/services/tweeterService.ts): use object user from db to connect to twitter API

--- server/src/services/tweeterService.ts
+++ server/src/services/tweeterService.ts
@@ -1,6 +1,7 @@
 import { env } from "process";
 
 import TwitterApi from "twitter-api-v2";
+import User from "@classes/user.class";
 
 // doc :
 // https://www.npmjs.com/package/twitter-v2
@@ -8,19 +9,28 @@ import TwitterApi from "twitter-api-v2";
 
 // https://api.twitter.com/2/users/:id/tweets
 
-export async function GetUserLastTweet(username: string): Promise<void> {
+export async function GetUserLastTweet(username: string, user: User): Promise<boolean> {
 
-    if (!env.TWITTER_BEARER_TOKEN)
-        return;
-    const client = new TwitterApi(env.TWITTER_BEARER_TOKEN);
-    const user = await client.v2.userByUsername(username);
-    console.log(user);
+    if (!env.TWITTER_API_KEY || !env.TWITTER_API_SECRET_KEY || !user.oauth.twitter)
+        throw "Invalid twitter credentials";
+    const client = new TwitterApi({
+        appKey: env.TWITTER_API_KEY,
+        appSecret: env.TWITTER_API_SECRET_KEY,
+        accessToken: user.oauth.twitter?.accessToken,
+        accessSecret: user.oauth.twitter?.secretToken
+    });
+    const userTweeting = await client.v2.userByUsername(username);
+    console.log(userTweeting);
 
-    const userTimeline = await client.v2.userTimeline(user.data.id, {
+    const userTimeline = await client.v2.userTimeline(userTweeting.data.id, {
         expansions: ["attachments.media_keys", "attachments.poll_ids", "referenced_tweets.id"],
         "media.fields": ["url"]
     });
-    for await (const tweet of userTimeline) {
+    const tweet = userTimeline[0];
+    if (tweet.referenced_tweets && tweet.referenced_tweets["type"] != "tweeted")
+        return false;
+    return true;
+    /* for await (const tweet of userTimeline) {
         const medias = userTimeline.includes.medias(tweet);
         const poll = userTimeline.includes.poll(tweet);
         if (tweet.referenced_tweets && tweet.referenced_tweets["type"] != "tweeted")
@@ -35,42 +45,43 @@ export async function GetUserLastTweet(username: string): Promise<void> {
             console.log("This tweet contains a poll! Options:", poll.options.map(opt => opt.label));
         break;
     }
+    return false;*/
 }
 
-export async function TweetATweet(text: string): Promise<void> {
-    if (!env.TWITTER_API_KEY || !env.TWITTER_API_SECRET_KEY)
-        return;
+export async function TweetATweet(text: string, user: User): Promise<void> {
+    if (!env.TWITTER_API_KEY || !env.TWITTER_API_SECRET_KEY || !user.oauth.twitter)
+        throw "Invalid twitter credentials";
     const client = new TwitterApi({
         appKey: env.TWITTER_API_KEY,
         appSecret: env.TWITTER_API_SECRET_KEY,
-        accessToken: "", // TODO replace with DB call
-        accessSecret: "" // TODO replace with DB call
+        accessToken: user.oauth.twitter?.accessToken,
+        accessSecret: user.oauth.twitter?.secretToken
     });
 
     client.v2.tweet(text);
 }
 
-export async function UpdateProfileBanner(imagePath: string): Promise<void> {
-    if (!env.TWITTER_API_KEY || !env.TWITTER_API_SECRET_KEY)
-        return;
+export async function UpdateProfileBanner(imagePath: string, user: User): Promise<void> {
+    if (!env.TWITTER_API_KEY || !env.TWITTER_API_SECRET_KEY || !user.oauth.twitter)
+        throw "Invalid twitter credentials";
     const client = new TwitterApi({
         appKey: env.TWITTER_API_KEY,
         appSecret: env.TWITTER_API_SECRET_KEY,
-        accessToken: "", // TODO replace with DB call
-        accessSecret: "" // TODO replace with DB call
+        accessToken: user.oauth.twitter?.accessToken,
+        accessSecret: user.oauth.twitter?.secretToken
     });
 
     client.v1.updateAccountProfileBanner(imagePath);
 }
 
-export async function UpdateProfileImage(imagePath: string): Promise<void> {
+export async function UpdateProfileImage(imagePath: string, user: User): Promise<void> {
     if (!env.TWITTER_API_KEY || !env.TWITTER_API_SECRET_KEY)
-        return;
+        throw "Invalid twitter credentials";
     const client = new TwitterApi({
         appKey: env.TWITTER_API_KEY,
         appSecret: env.TWITTER_API_SECRET_KEY,
-        accessToken: "", // TODO replace with DB call
-        accessSecret: "" // TODO replace with DB call
+        accessToken: user.oauth.twitter?.accessToken,
+        accessSecret: user.oauth.twitter?.secretToken
     });
 
     client.v1.updateAccountProfileImage(imagePath);
