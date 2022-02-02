f3d4aa5907210d2effbe89a0173832e50ed2f257
Arthur <arthur.jourdan@epitech.eu>
2022-02-02 22:21:32 +0100

    feat(src/services/DropboxService.ts): class and methods to upload file to dropbox or get a list of folders

--- server/src/services/DropboxService.ts
+++ server/src/services/DropboxService.ts
@@ -1,22 +1,51 @@
 import { env } from "process";
-import { Dropbox } from "dropbox";
+import { Dropbox, files } from "dropbox";
+import { readFile } from "fs";
 
 // import User from "../classes/user.class";
 
 const token = "";
 
-export function uploadFile(filepath: string/* , user: User */) {
+export class DropboxService {
 
-    if (!env.DROPBOX_API_KEY || !env.DROPBOX_API_SECRET_KEY)
-        return;
+    client: Dropbox | null = null;
 
-    const dbx = new Dropbox({ clientId: env.DROPBOX_API_KEY, clientSecret: env.DROPBOX_API_SECRET_KEY, accessToken: token /* user.ser.oauth.dropbox?.secretToken */ });
-    dbx.filesListFolder({ path: "" })
-        .then(function (response) {
-            console.log(response.result.entries.at(0));
-        })
-        .catch(function (error) {
-            console.log(error);
+    constructor(/* user: User */) {
+        if (!env.DROPBOX_API_KEY || !env.DROPBOX_API_SECRET_KEY /* || !user.oauth.dropbox */)
+            return;
+        this.client = new Dropbox({ clientId: env.DROPBOX_API_KEY, clientSecret: env.DROPBOX_API_SECRET_KEY, accessToken: token /* user.oauth.dropbox?.secretToken */ });
+    }
+
+    uploadFile(filepath: string, dropboxFilepath: string) {
+
+        readFile(filepath, "utf-8", (err, contents) => {
+            if (err)
+                console.log("Error: ", err);
+
+            dropboxFilepath = "/my_folder/" + dropboxFilepath;
+            if (!this.client)
+                return;
+            this.client.filesUpload({ path: dropboxFilepath, contents })
+                .then((response: any) => {
+                    console.log(response);
+                });
+            // .catch((uploadErr: Error<files.UploadError>) => {
+            // console.log(uploadErr);
+            // });
         });
-    filepath;
+    }
+
+    listFolders(path = "") {
+
+        if (!this.client)
+            return;
+        this.client.filesListFolder({ path: path })
+            .then(function (response) {
+                console.log(response.result.entries.at(0));
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+    }
 }
