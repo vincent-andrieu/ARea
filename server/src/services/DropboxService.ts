462b8853ea2e4bfe53ae072e3c57beb8266e6b63
Arthur <arthur.jourdan@epitech.eu>
2022-02-03 09:40:02 +0100

    feat(src/services/DropboxService.ts): method to set client

--- server/src/services/DropboxService.ts
+++ server/src/services/DropboxService.ts
@@ -4,25 +4,27 @@ import { readFile } from "fs";
 
 // import User from "../classes/user.class";
 
-const token = "";
+const token = "sl.BBSyaeyLST13F7QqeGQQFgBtwWUUAVUPzLhaY46CsJVHSYAojEc8JETW_W3x9Ds1it9ejAI2J8JF41SZ-ocQQmDMwtQlD3DLKH2lH0Y6pd_6IitoPi17XiyV3EFwWljPuNqGpfg";
 
 export class DropboxService {
 
     client: Dropbox | null = null;
 
-    constructor(/* user: User */) {
+    constructor() {
+        this.setClient();
+    }
+
+    setClient(/* user: User */) {
         if (!env.DROPBOX_API_KEY || !env.DROPBOX_API_SECRET_KEY /* || !user.oauth.dropbox */)
             return;
         this.client = new Dropbox({ clientId: env.DROPBOX_API_KEY, clientSecret: env.DROPBOX_API_SECRET_KEY, accessToken: token /* user.oauth.dropbox?.secretToken */ });
     }
-
     uploadFile(filepath: string, dropboxFilepath: string) {
 
         readFile(filepath, "utf-8", (err, contents) => {
             if (err)
                 console.log("Error: ", err);
 
-            dropboxFilepath = "/my_folder/" + dropboxFilepath;
             if (!this.client)
                 return;
             this.client.filesUpload({ path: dropboxFilepath, contents })
