45828faa752a9c05384d332c777ff0732ea593d2
Vincent Andrieu <vincent.andrieu@epitech.eu>
2022-02-07 14:45:09 +0100

    Fix /user route

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -15,7 +15,7 @@ export default class UserController {
 
             const result = await UserController._userSchema.get(userId);
 
-            res.status(200).send(result);
+            res.status(200).send(result.toRaw());
         } catch (error: any) {
             console.log(error.toString());
             return res.status(400).send(error.toString());
