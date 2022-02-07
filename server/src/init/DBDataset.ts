3062392b71ff096be1d671018ea449fbe6d9ad4c
simon.racaud <simon.racaud@epitech.eu>
2022-02-07 18:14:24 +0100

    feat(server): load database default dataset

---
+++ server/src/init/DBDataset.ts
@@ -0,0 +1,26 @@
+import { ActionSchema } from "../schemas/action.schema";
+import { ReactionSchema } from "../schemas/reaction.schema";
+import mongoose from "mongoose";
+import Database from "../init/database";
+import Action from "../classes/action.class";
+
+import actionDataset from "../../../db/actions.json";
+import reactionDataset from "../../../db/reactions.json";
+import Reaction from "../classes/reaction.class";
+
+export default class DBDataset {
+    private static _actionSchema = new ActionSchema();
+    private static _reactionSchema = new ReactionSchema();
+
+    public static async load() {
+        await mongoose.connection.dropDatabase();
+        await Database.connect();
+
+        actionDataset.forEach((item) => {
+            this._actionSchema.add(item as Action);
+        });
+        reactionDataset.forEach((item) => {
+            this._reactionSchema.add(item as Reaction);
+        });
+    }
+}
