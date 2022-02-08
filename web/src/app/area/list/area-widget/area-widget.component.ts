4a7e27aad0a0ae3382334d1f0fddb0157da8095f
Vincent Andrieu <vincent.andrieu@epitech.eu>
2022-02-08 17:14:15 +0100

    Fix browser compilation

--- web/src/app/area/list/area-widget/area-widget.component.ts
+++ web/src/app/area/list/area-widget/area-widget.component.ts
@@ -31,40 +31,40 @@ export class AreaWidgetComponent implements OnInit {
     constructor(private _authService: AuthService) {}
 
     ngOnInit(): void {
-        // if (!this.area || isObjectId(this.area.trigger.action) || isObjectId(this.area.consequence.reaction))
-        //     throw "Invalid area";
+        if (!this.area || isObjectId(this.area.trigger.action) || isObjectId(this.area.consequence.reaction))
+            throw "Invalid area";
 
-        // this.displayableAreaAction = {
-        //     title: this._getActionTitle(this.area.trigger.action.type),
-        //     subtitle: this._getActionSubtitle(this.area.trigger),
-        //     svgPath: this._getSvgPath(this.area.trigger.action.type)
-        // };
-        // this.displayableAreaReaction = {
-        //     title: this._getReactionTitle(this.area.consequence.reaction.type),
-        //     subtitle: this._getReactionSubtitle(this.area.consequence),
-        //     svgPath: this._getSvgPath(this.area.consequence.reaction.type)
-        // };
+        this.displayableAreaAction = {
+            title: this._getActionTitle(this.area.trigger.action.type),
+            subtitle: this._getActionSubtitle(this.area.trigger),
+            svgPath: this._getSvgPath(this.area.trigger.action.type)
+        };
+        this.displayableAreaReaction = {
+            title: this._getReactionTitle(this.area.consequence.reaction.type),
+            subtitle: this._getReactionSubtitle(this.area.consequence),
+            svgPath: this._getSvgPath(this.area.consequence.reaction.type)
+        };
     }
 
     private _getActionTitle(type: ActionType): string {
         switch (type) {
-        case ActionType.DATETIME:
+        case 'DATETIME':
             return "Date";
-        case ActionType.DATE:
+        case 'DATE':
             return "CRON";
-        case ActionType.DISCORD_MSG:
+        case 'DISCORD_MSG':
             return "Send message";
-        case ActionType.GITHUB_ISSUE:
+        case 'GITHUB_ISSUE':
             return "GitHub issue";
-        case ActionType.GITHUB_PULL_REQ:
+        case 'GITHUB_PULL_REQ':
             return "GitHub pull request";
-        case ActionType.RSS_ENTRY:
+        case 'RSS_ENTRY':
             return "RSS";
-        case ActionType.TWITCH_STREAM:
+        case 'TWITCH_STREAM':
             return "Twitch stream";
-        case ActionType.TWITTER_MSG:
+        case 'TWITTER_MSG':
             return "Twitter message";
-        case ActionType.UNSPLASH_POST:
+        case 'UNSPLASH_POST':
             return "Post on Unsplash";
 
         default: {
@@ -79,23 +79,23 @@ export class AreaWidgetComponent implements OnInit {
             throw "Invalid area action";
 
         switch (trigger.action.type) {
-        case ActionType.DATETIME:
+        case 'DATETIME':
             return moment((trigger.inputs as DateTimeConfig).time).format('LLL');
-        case ActionType.DATE:
+        case 'DATE':
             return (trigger.inputs as TimeConfig).time;
-        case ActionType.DISCORD_MSG:
+        case 'DISCORD_MSG':
             return (trigger.inputs as DiscordMessageConfig).channelId;
-        case ActionType.GITHUB_ISSUE:
+        case 'GITHUB_ISSUE':
             return (trigger.inputs as GithubIssueConfig).owner + "/" + (trigger.inputs as GithubIssueConfig).repository;
-        case ActionType.GITHUB_PULL_REQ:
+        case 'GITHUB_PULL_REQ':
             return (trigger.inputs as GithubPullReqConfig).owner + "/" + (trigger.inputs as GithubPullReqConfig).repository;
-        case ActionType.RSS_ENTRY:
+        case 'RSS_ENTRY':
             return (trigger.inputs as RSSConfig).url;
-        case ActionType.TWITCH_STREAM:
+        case 'TWITCH_STREAM':
             return (trigger.inputs as TwitchStreamConfig).username;
-        case ActionType.TWITTER_MSG:
+        case 'TWITTER_MSG':
             return (trigger.inputs as TwitterTweetConfig).username;
-        case ActionType.UNSPLASH_POST:
+        case 'UNSPLASH_POST':
             return (trigger.inputs as UnsplashPostConfig).username;
 
         default: {
@@ -107,23 +107,23 @@ export class AreaWidgetComponent implements OnInit {
 
     private _getReactionTitle(type: ReactionType): string {
         switch (type) {
-        case ReactionType.TWITTER_MSG:
+        case 'TWITTER_MSG':
             return "Send Twitter message";
-        case ReactionType.TWITTER_BANNER:
+        case 'TWITTER_BANNER':
             return "Edit Twitter banner";
-        case ReactionType.TWITTER_PP:
+        case 'TWITTER_PP':
             return "Edit Twitter PP";
-        case ReactionType.LINKEDIN_MSG:
+        case 'LINKEDIN_MSG':
             return "Send Linkedin message";
-        case ReactionType.DISCORD_MSG:
+        case 'DISCORD_MSG':
             return "Send Discord message";
-        case ReactionType.GITHUB_ISSUE:
+        case 'GITHUB_ISSUE':
             return "Add GitHub issue";
-        case ReactionType.NOTION_MSG:
+        case 'NOTION_MSG':
             return "Add Notion message";
-        case ReactionType.DROPBOX_UPLOAD:
+        case 'DROPBOX_UPLOAD':
             return "Upload on Dropbox";
-        case ReactionType.UNSPLASH:
+        case 'UNSPLASH':
             return "Post on Unsplash";
 
         default: {
@@ -138,23 +138,23 @@ export class AreaWidgetComponent implements OnInit {
             throw "Invalid area reaction";
 
         switch (consequence.reaction.type) {
-        case ReactionType.TWITTER_MSG:
+        case 'TWITTER_MSG':
             return (consequence.inputs as TwitterPostTweetConfig).message;
-        case ReactionType.TWITTER_BANNER:
+        case 'TWITTER_BANNER':
             return (consequence.inputs as TwitterUpdatePictureConfig).nothing;
-        case ReactionType.TWITTER_PP:
+        case 'TWITTER_PP':
             return (consequence.inputs as TwitterUpdatePictureConfig).nothing;
-        case ReactionType.LINKEDIN_MSG:
+        case 'LINKEDIN_MSG':
             return (consequence.inputs as LinkedinPostConfig).message;
-        case ReactionType.DISCORD_MSG:
+        case 'DISCORD_MSG':
             return (consequence.inputs as DiscordPostMsgConfig).message;
-        case ReactionType.GITHUB_ISSUE:
+        case 'GITHUB_ISSUE':
             return (consequence.inputs as GithubCreateIssueConfig).owner + "/" + (consequence.inputs as GithubCreateIssueConfig).repository;
-        case ReactionType.NOTION_MSG:
+        case 'NOTION_MSG':
             return (consequence.inputs as NotionAddMessageConfig).urlPage;
-        case ReactionType.DROPBOX_UPLOAD:
+        case 'DROPBOX_UPLOAD':
             return (consequence.inputs as DropboxUploadConfig).remoteFilepath || "Undefined";
-        case ReactionType.UNSPLASH:
+        case 'UNSPLASH':
             return (consequence.inputs as any);
 
         default: {
