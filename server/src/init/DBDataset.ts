aeeea3005726661e47d13a7759ff2bb52f6d87a3
simon.racaud <simon.racaud@epitech.eu>
2022-02-08 18:30:02 +0100

    feat(server): CRON and DATETIME actions

--- server/src/init/DBDataset.ts
+++ server/src/init/DBDataset.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-import Action from "@classes/action.class";
+import Action, { ActionType } from "@classes/action.class";
 import Reaction from "@classes/reaction.class";
 import { ActionSchema } from "@schemas/action.schema";
 import { ReactionSchema } from "@schemas/reaction.schema";
@@ -19,13 +19,26 @@ export default class DBDataset {
             await this._removeCollection("users");
             await this._removeCollection("areas");
         }
-        await mongoose.connection.dropCollection("actions");
-        await mongoose.connection.dropCollection("reactions");
-        actionDataset.forEach((item) => {
-            this._actionSchema.add(item as Action);
+        actionDataset.forEach(async (item) => {
+            try {
+                const result = await this._actionSchema.find({ type: item.type });
+
+                if (result.length === 0)
+                    this._actionSchema.add(item as Action);
+
+            } catch (e: any) {
+                console.error(e.message);
+            }
         });
-        reactionDataset.forEach((item) => {
-            this._reactionSchema.add(item as Reaction);
+        reactionDataset.forEach(async (item) => {
+            try {
+                const result = await this._actionSchema.find({ type: item.service });
+
+                if (result.length === 0)
+                    this._reactionSchema.add(item as Reaction);
+            } catch (e: any) {
+                console.error(e.message);
+            }
         });
     }
 
--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,7 @@ import "module-alias/register";
 
 import Express from "./init/express";
 import DBDataset from "./init/DBDataset";
-import { CronService } from "./services/CronService";
+import CronService from "./services/CronService";
 
 // Connection to the database and loading of the dataset.
 DBDataset.init(false);
