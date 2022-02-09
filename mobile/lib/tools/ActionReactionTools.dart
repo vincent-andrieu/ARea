d90397a2ff62c512eb3f154d9ceefef552ae7f9c
Sullivan Geslot <sullivan.geslot@epitech.eu>
2022-02-09 11:18:48 +0100

    - Fix some mistake

--- mobile/lib/tools/ActionReactionTools.dart
+++ mobile/lib/tools/ActionReactionTools.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:mobile/api/model/area/ActionType.dart';
 import 'package:mobile/api/model/area/ReactionType.dart';
 import 'package:mobile/service/IService.dart';
@@ -14,14 +16,31 @@ import 'package:mobile/service/unsplash.dart';
 
 ActionType getActionTypeByDescr(String desc) {
   Map<String, ActionType> map = {
-
+    'Date': ActionType.DATE,
+    'Date time': ActionType.DATETIME,
+    'New stream started': ActionType.TWITCH_STREAM,
+    'On new tweet': ActionType.TWITTER_MSG,
+    'Rss entry': ActionType.RSS_ENTRY,
+    'New Issue': ActionType.GITHUB_ISSUE,
+    'New Pull Request': ActionType.GITHUB_PULL_REQ,
+    'New message on group': ActionType.DISCORD_MSG,
+    'Unsplash post': ActionType.UNSPLASH_POST,
   };
+  log(desc);
   return map[desc]!;
 }
 
 ReactionType getReactionTypeByDescr(String desc) {
   Map<String, ReactionType> map = {
-
+    'Make a tweet': ReactionType.TWITTER_MSG,
+    'Change Banner': ReactionType.TWITTER_BANNER,
+    'Update profile image': ReactionType.TWITTER_PP,
+    'Publish post': ReactionType.LINKEDIN_MSG,
+    'Send message': ReactionType.DISCORD_MSG,
+    'Create new Issue': ReactionType.GITHUB_ISSUE,
+    'Update Notion Database': ReactionType.NOTION_MSG,
+    "Upload file": ReactionType.DROPBOX_UPLOAD,
+    "Unsplash": ReactionType.UNSPLASH,
   };
   return map[desc]!;
 }
--- server/src/services/twitchService.ts
+++ server/src/services/twitchService.ts
@@ -51,7 +51,7 @@ export class TwitchService {
 
         const clientId = env.TWITCH_CLIENT_ID;
         const clientSecret = env.TWITCH_CLIENT_SECRET;
-        const redirectUri = env.TWITCH_CALLBACK_URL;
+        const redirectUri = env.TWITCH_CALLBACK_MOBILE;
         if (!clientId || !clientSecret || !redirectUri)
             return;
 
@@ -67,6 +67,7 @@ export class TwitchService {
             const response = await axios.post(`${url}?${params}`);
             return response.data;
         } catch (error) {
+            console.log("[TWITCH]:", (error as any).response.data.message);
             console.log("[TWITCH] getAccessToken: ", (error as Error).toString());
             return;
         }
