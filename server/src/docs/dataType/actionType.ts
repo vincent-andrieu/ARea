d40ac7c41d33f6384913c144b75416e3af49f61c
Arthur <arthur.jourdan@epitech.eu>
2022-02-09 12:09:47 +0100

    docs(src/docs/dataType/actionType.ts): update action type with new 'unsplash random post' action

--- server/src/docs/dataType/actionType.ts
+++ server/src/docs/dataType/actionType.ts
@@ -3,7 +3,7 @@ export const actionType = {
         type: "string",
         enum: ["CRON", "DATETIME", "TWITCH_STREAM",
             "TWITTER_MSG", "RSS_ENTRY", "GITHUB_ISSUE",
-            "GITHUB_PULL_REQ", "DISCORD_MSG", "UNSPLASH_POST"
+            "GITHUB_PULL_REQ", "DISCORD_MSG", "UNSPLASH_POST", "UNSPLASH_RANDOM_POST"
         ]
     }
 };
