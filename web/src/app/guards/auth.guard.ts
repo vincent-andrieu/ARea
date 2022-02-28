bc890b7c1afc926636b09dfc91d3ac0e4900d87a
Vincent Andrieu <vincent.andrieu@epitech.eu>
2022-02-28 16:42:19 +0100

    Add username in tab name

--- web/src/app/guards/auth.guard.ts
+++ web/src/app/guards/auth.guard.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, UrlTree } from '@angular/router';
 import { HttpClient } from "@angular/common/http";
+import { Title } from "@angular/platform-browser";
 import { catchError, Observable, of } from 'rxjs';
 import { CookieService } from "ngx-cookie";
 
 import { environment } from "@environment";
+import User from "@classes/user.class";
 
 /**
  * Check if the user is login
@@ -15,6 +17,7 @@ import { environment } from "@environment";
 export class AuthGuard implements CanActivate {
 
     constructor(
+        private _title: Title,
         private _router: Router,
         private _httpClient: HttpClient,
         private _cookieService: CookieService
@@ -28,9 +31,15 @@ export class AuthGuard implements CanActivate {
 
         if (this._cookieService.hasKey(environment.cookies.jwt.name))
             return new Promise<boolean | UrlTree>((resolve) => {
-                this._httpClient.get('/')
+                this._httpClient.get<User>('/user')
                     .pipe(catchError(() => of(resolve(this._router.parseUrl('/login')))))
-                    .subscribe(() => resolve(true));
+                    .subscribe((user: User | void) => {
+                        if (user)
+                            this._title.setTitle("ARea - " + user.username);
+                        else
+                            this._title.setTitle("ARea");
+                        resolve(true);
+                    });
             });
         return this._router.parseUrl('/login');
     }
