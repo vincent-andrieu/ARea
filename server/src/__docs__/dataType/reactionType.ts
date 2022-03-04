9e7932b2f2c97ac88179b5980569825e989bfa76
Arthur <arthur.jourdan@epitech.eu>
2022-03-04 14:13:44 +0100

    docs(src/__docs__/dataType/reactionType.ts): add and remove reaction names in documentation

--- server/src/__docs__/dataType/reactionType.ts
+++ server/src/__docs__/dataType/reactionType.ts
@@ -1,8 +1,8 @@
 export const reactionType = {
     ReactionType: {
         type: "string",
-        enum: ["TWITTER_MSG", "LINKEDIN_MSG", "TWITTER_PP",
-            "DISCORD_MSG", "GITHUB_ISSUE", "NOTION_MSG",
+        enum: ["TWITTER_MSG", "TWITTER_PP", "TWITTER_BANNER",
+            "DISCORD_MSG", "GITHUB_ISSUE", "GITHUB_PULL_REQ", "NOTION_MSG",
             "DROPBOX_UPLOAD"
         ]
     }
